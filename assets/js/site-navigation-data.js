4b0fb5ac081ce69416539f9b6d51ca010477140e
ethereoninitiative <suntoad@me.com>
2026-05-17 12:17:36 -0700

    Add shared site navigation data

---
+++ assets/js/site-navigation-data.js
@@ -0,0 +1,25 @@
+window.ETHEREON_SITE_NAVIGATION = {
+  primary: [
+    ['index.html', 'Home'],
+    ['lumina.html', 'Lumina'],
+    ['continuity.html', 'Continuity'],
+    ['roadmap.html', 'Roadmap'],
+    ['harmonics.html', 'Harmonics'],
+    ['rse.html', 'RSE'],
+    ['rse-whitepaper.html', 'The Spiral'],
+    ['explore.html', 'Explore'],
+    ['chamber.html', 'Chamber'],
+    ['about.html', 'About'],
+    ['contact.html', 'Contact']
+  ],
+  secondaryFooter: [
+    ['principles.html', 'Principles'],
+    ['realm.html', 'Realm'],
+    ['lumina-dashboard.html', 'Dashboard'],
+    ['build.html', 'Build'],
+    ['specimen.html', 'Specimen'],
+    ['lexicon.html', 'Lexicon'],
+    ['faq.html', 'FAQ'],
+    ['updates.html', 'Updates']
+  ]
+};
